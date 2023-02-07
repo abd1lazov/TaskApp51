6ee1b6acef3e705f6d34df5b60bf3f628d1ccf2e
abdykadyr <abdukadyrabdilazov@icloud.com>
2023-02-07 18:03:09 +0100

    add library

--- app/src/main/java/com/example/taskapp51/ui/authentication/AuthFragment.kt
+++ app/src/main/java/com/example/taskapp51/ui/authentication/AuthFragment.kt
@@ -6,16 +6,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.taskapp51.databinding.FragmentAuthBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class AuthFragment : Fragment() {
 
     private lateinit var binding: FragmentAuthBinding
+    private var auth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentAuthBinding.inflate(inflater,container,false)
+
+        initViews()
+        initListeners()
+
         return binding.root
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initViews() {
+        auth.setLanguageCode("ru")
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(binding.etPhone.text.toString())
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(object: PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
     }
 }
