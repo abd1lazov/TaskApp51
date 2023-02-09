8242482bec324d27cf2e210045d9a1a45a5e6cbc
abdykadyr <abdukadyrabdilazov@icloud.com>
2023-02-09 18:11:02 +0100

    change Auth fragment

--- app/src/main/java/com/example/taskapp51/ui/authentication/AuthFragment.kt
+++ app/src/main/java/com/example/taskapp51/ui/authentication/AuthFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import com.example.taskapp51.databinding.FragmentAuthBinding
+import com.example.taskapp51.extensions.showToast
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
@@ -17,6 +19,7 @@ class AuthFragment : Fragment() {
 
     private lateinit var binding: FragmentAuthBinding
     private var auth = FirebaseAuth.getInstance()
+    private var correctCode: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,12 +45,19 @@ class AuthFragment : Fragment() {
             .setActivity(requireActivity())
             .setCallbacks(object: PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                 override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-
+//                    showToast(p0.smsCode.toString())
                 }
 
-                override fun onVerificationFailed(p0: FirebaseException) {
+                override fun onVerificationFailed(exception: FirebaseException) {
+                    showToast(exception.message.toString())
                 }
 
+                override fun onCodeSent(verificationCode: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(verificationCode, p1)
+                    correctCode = verificationCode
+
+                    binding.etPhone.isVisible = false
+                }
             })
             .build()
         PhoneAuthProvider.verifyPhoneNumber(options)
